1e8c97cf2c96f9bb11588ef72524131eafb405b5
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-12-02 12:06:11 -0600

    gitignore

--- app.py
+++ app.py
@@ -22,23 +22,17 @@ from dash.dependencies import Input, Output
 from flask import Flask
 import pkg_resources
 
-# backup true function
 _true_get_distribution = pkg_resources.get_distribution
-# create small placeholder for the dash call
-# _flask_compress_version = parse_version(get_distribution("flask-compress").version)
 _Dist = namedtuple('_Dist', ['version'])
-
 
 def _get_distribution(dist):
     if dist == 'flask-compress':
-        return _Dist('1.9.0') # your flask-compress version
+        return _Dist('1.9.0') 
     else:
         return _true_get_distribution(dist)
 
-
-# monkey patch the function so it can work once frozen and pkg_resources is of
-# no help
 pkg_resources.get_distribution = _get_distribution
+
 server = Flask(__name__)
 
 app = dash.Dash(__name__,
